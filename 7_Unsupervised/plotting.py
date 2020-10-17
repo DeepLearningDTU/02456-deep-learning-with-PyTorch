3bb9d738fe9f66ba2f8fccfd9d9c4cf1561e0870
Valentin Liévin <valentin.lievin@gmail.com>
2020-10-17 16:51:26 +0200

    Simplified imports

---
+++ 7_Unsupervised/plotting.py
@@ -0,0 +1,247 @@
+import os
+from typing import *
+
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+import torch
+from IPython.display import Image, display, clear_output
+from sklearn.manifold import TSNE
+from torch import Tensor
+from torch.distributions import Normal
+from torchvision.utils import make_grid
+
+
+def plot_autoencoder_stats(
+        x: Tensor = None,
+        x_hat: Tensor = None,
+        z: Tensor = None,
+        y: Tensor = None,
+        epoch: int = None,
+        train_loss: List = None,
+        valid_loss: List = None,
+        classes: List = None,
+        dimensionality_reduction_op: Optional[Callable] = None,
+) -> None:
+    """
+    An utility 
+    """
+    # -- Plotting --
+    f, axarr = plt.subplots(2, 2, figsize=(20, 20))
+
+    # Loss
+    ax = axarr[0, 0]
+    ax.set_title("Error")
+    ax.set_xlabel('Epoch')
+    ax.set_ylabel('Error')
+
+    ax.plot(np.arange(epoch + 1), train_loss, color="black")
+    ax.plot(np.arange(epoch + 1), valid_loss, color="gray", linestyle="--")
+    ax.legend(['Training error', 'Validation error'])
+
+    # Latent space
+    ax = axarr[0, 1]
+
+    ax.set_title('Latent space')
+    ax.set_xlabel('Dimension 1')
+    ax.set_ylabel('Dimension 2')
+
+    # If you want to use a dimensionality reduction method you can use
+    # for example TSNE by projecting on two principal dimensions
+    # TSNE.fit_transform(z)
+    if dimensionality_reduction_op is not None:
+        z = dimensionality_reduction_op(z)
+
+    colors = iter(plt.get_cmap('Set1')(np.linspace(0, 1.0, len(classes))))
+    for c in classes:
+        ax.scatter(*z[y.numpy() == c].T, c=next(colors), marker='o')
+
+    ax.legend(classes)
+
+    # Inputs
+    ax = axarr[1, 0]
+    ax.set_title('Inputs')
+    ax.axis('off')
+
+    rows = 8
+    batch_size = x.size(0)
+    columns = batch_size // rows
+
+    canvas = np.zeros((28 * rows, columns * 28))
+    for i in range(rows):
+        for j in range(columns):
+            idx = i % columns + rows * j
+            canvas[i * 28:(i + 1) * 28, j * 28:(j + 1) * 28] = x[idx].reshape((28, 28))
+    ax.imshow(canvas, cmap='gray')
+
+    # Reconstructions
+    ax = axarr[1, 1]
+    ax.set_title('Reconstructions')
+    ax.axis('off')
+
+    canvas = np.zeros((28 * rows, columns * 28))
+    for i in range(rows):
+        for j in range(columns):
+            idx = i % columns + rows * j
+            canvas[i * 28:(i + 1) * 28, j * 28:(j + 1) * 28] = x_hat[idx].reshape((28, 28))
+
+    ax.imshow(canvas, cmap='gray')
+
+    tmp_img = "tmp_ae_out.png"
+    plt.savefig(tmp_img)
+    plt.close(f)
+    display(Image(filename=tmp_img))
+    clear_output(wait=True)
+
+    os.remove(tmp_img)
+
+
+def plot_samples(ax, x):
+    x = x.to('cpu')
+    nrow = int(np.sqrt(x.size(0)))
+    x_grid = make_grid(x.view(-1, 1, 28, 28), nrow=nrow).permute(1, 2, 0)
+    ax.imshow(x_grid)
+    ax.axis('off')
+
+
+def plot_interpolations(ax, vae):
+    nrow = 10
+    nsteps = 10
+    prior_params = vae.prior_params.expand(2 * nrow, *vae.prior_params.shape[-1:])
+    mu, log_sigma = prior_params.chunk(2, dim=-1)
+    pz = Normal(mu, log_sigma.exp())
+    z = pz.sample().view(nrow, 2, -1).cpu()
+    t = torch.linspace(0, 1, 10)
+    zs = t[None, :, None] * z[:, 0, None, :] + (1 - t[None, :, None]) * z[:, 1, None, :]
+    px = vae.observation_model(zs.view(nrow * nsteps, -1))
+    x = px.sample().cpu()
+    x_grid = make_grid(x.view(-1, 1, 28, 28), nrow=nrow).permute(1, 2, 0)
+    ax.imshow(x_grid)
+    ax.axis('off')
+
+
+def plot_grid(ax, vae):
+    nrow = 10
+    xv, yv = torch.meshgrid([torch.linspace(-3, 3, 10), torch.linspace(-3, 3, 10)])
+    zs = torch.cat([xv[:, :, None], yv[:, :, None]], -1)
+    px = vae.observation_model(zs.view(nrow * nrow, 2))
+    x = px.sample().cpu()
+    x_grid = make_grid(x.view(-1, 1, 28, 28), nrow=nrow).permute(1, 2, 0)
+    ax.imshow(x_grid)
+    ax.axis('off')
+
+
+def plot_2d_latents(ax, qz, z, y):
+    z = z.to('cpu')
+    y = y.yo('cpu')
+    scale_factor = 2
+    batch_size = z.shape[0]
+    palette = sns.color_palette()
+    colors = [palette[l] for l in y]
+
+    # plot prior
+    prior = plt.Circle((0, 0), scale_factor, color='gray', fill=True, alpha=0.1)
+    ax.add_artist(prior)
+
+    # plot data points
+    mus, sigmas = qz.mu.to('cpu'), qz.sigma.to('cpu')
+    mus = [mus[i].numpy().tolist() for i in range(batch_size)]
+    sigmas = [sigmas[i].numpy().tolist() for i in range(batch_size)]
+
+    posteriors = [
+        plt.matplotlib.patches.Ellipse(mus[i], *(scale_factor * s for s in sigmas[i]), color=colors[i], fill=False,
+                                       alpha=0.3) for i in range(batch_size)]
+    for p in posteriors:
+        ax.add_artist(p)
+
+    ax.scatter(z[:, 0], z[:, 1], color=colors)
+
+    ax.set_xlim([-3, 3])
+    ax.set_ylim([-3, 3])
+    ax.set_aspect('equal', 'box')
+
+
+def plot_latents(ax, z, y):
+    z = z.to('cpu')
+    palette = sns.color_palette()
+    colors = [palette[l] for l in y]
+    z = TSNE(n_components=2).fit_transform(z)
+    ax.scatter(z[:, 0], z[:, 1], color=colors)
+
+
+def make_vae_plots(vae, x, y, outputs, training_data, validation_data, tmp_img="tmp_vae_out.png", figsize=(18, 18)):
+    fig, axes = plt.subplots(3, 3, figsize=figsize, squeeze=False)
+
+    # plot the observation
+    axes[0, 0].set_title(r'Observation $\mathbf{x}$')
+    plot_samples(axes[0, 0], x)
+
+    # plot the latent samples
+    try:
+        z = outputs['z']
+        if z.shape[1] == 2:
+            axes[0, 1].set_title(r'Latent Samples $\mathbf{z} \sim q_\phi(\mathbf{z} | \mathbf{x})$')
+            qz = outputs['qz']
+            plot_2d_latents(axes[0, 1], qz, z, y)
+        else:
+            axes[0, 1].set_title(r'Latent Samples $\mathbf{z} \sim q_\phi(\mathbf{z} | \mathbf{x})$ (t-SNE)')
+            plot_latents(axes[0, 1], z, y)
+    except Exception as e:
+        print(f"Could not generate the plot of the latent sanples because of exception")
+        print(e)
+
+    # plot posterior samples
+    axes[0, 2].set_title(
+        r'Reconstruction $\mathbf{x} \sim p_\theta(\mathbf{x} | \mathbf{z}), \mathbf{z} \sim q_\phi(\mathbf{z} | \mathbf{x})$')
+    px = outputs['px']
+    x_sample = px.sample().to('cpu')
+    plot_samples(axes[0, 2], x_sample)
+
+    # plot ELBO
+    ax = axes[1, 0]
+    ax.set_title(r'ELBO: $\mathcal{L} ( \mathbf{x} )$')
+    ax.plot(training_data['elbo'], label='Training')
+    ax.plot(validation_data['elbo'], label='Validation')
+    ax.legend()
+
+    # plot KL
+    ax = axes[1, 1]
+    ax.set_title(r'$\mathcal{D}_{\operatorname{KL}}\left(q_\phi(\mathbf{z}|\mathbf{x})\ |\ p(\mathbf{z})\right)$')
+    ax.plot(training_data['kl'], label='Training')
+    ax.plot(validation_data['kl'], label='Validation')
+    ax.legend()
+
+    # plot NLL
+    ax = axes[1, 2]
+    ax.set_title(r'$\log p_\theta(\mathbf{x} | \mathbf{z})$')
+    ax.plot(training_data['log_px'], label='Training')
+    ax.plot(validation_data['log_px'], label='Validation')
+    ax.legend()
+
+    # plot prior samples
+    axes[2, 0].set_title(r'Samples $\mathbf{x} \sim p_\theta(\mathbf{x} | \mathbf{z}), \mathbf{z} \sim p(\mathbf{z})$')
+    px = vae.sample_from_prior(batch_size=x.size(0))['px']
+    x_samples = px.sample()
+    plot_samples(axes[2, 0], x_samples)
+
+    # plot interpolations samples
+    axes[2, 1].set_title(
+        r'Latent Interpolations: $\mathbf{x} \sim p_\theta(\mathbf{x} | t \cdot \mathbf{z}_1 + (1-t) \cdot \mathbf{z}_2), \mathbf{z}_1, \mathbf{z}_2 \sim p(\mathbf{z}), t=0 \dots 1$')
+    plot_interpolations(axes[2, 1], vae)
+
+    # plot samples (sampling from a grid instead of the prior)
+    if vae.latent_features == 2:
+        axes[2, 2].set_title(
+            r'Samples: $\mathbf{x} \sim p_\theta(\mathbf{x} | \mathbf{z}), \mathbf{z} \sim \operatorname{grid}(-3:3, -3:3)$')
+        px = vae.sample_from_prior(batch_size=x.size(0))['px']
+        x_samples = px.sample()
+        plot_grid(axes[2, 2], vae)
+
+    # display
+    plt.tight_layout()
+    plt.savefig(tmp_img)
+    plt.close(fig)
+    display(Image(filename=tmp_img))
+    clear_output(wait=True)
+
+    os.remove(tmp_img)
